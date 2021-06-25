c2e8cba2f3236b5b1a7681a0d5166c01d748f0e1
Jimmy Jinghao Yao <46383546+Haowjy@users.noreply.github.com>
2021-06-25 12:48:29 -0400

    obtain and display sample api request in cards
    also remove unneeded files

--- api/api.py
+++ api/api.py
@@ -1,8 +1,52 @@
 import time
 from flask import Flask
+import datetime
+import pytz
+# my_date = datetime.datetime.now(pytz.timezone('US/New_York'))
+
+global counter
+counter = 0
 
 app = Flask(__name__)
 
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
+
+@app.route('/data')
+def get_sample_data():
+    global counter
+    counter += 1
+    return {
+        'time': datetime.datetime.now(pytz.timezone('America/New_York')),
+        'irradiance' : {
+            'global_horizontal': 0+counter,
+            'direct_normal' : 0+counter,
+            'diffuse_horizontal' : 0+counter,
+        },
+        'meteorological' : {
+            'pr1_temperature' : 0+counter,
+            'ph1_temperature': 0+counter,
+            'pressure' : 0+counter,
+            'zenith_angle' : 0+counter,
+            'azimuth_angle' : 0+counter,
+            'razon_status' : 0+counter,
+            'razon_time' : 0+counter,
+            'logger_battery' : 0+counter,
+            'logger_temp' : 0+counter,
+        },
+        'units' : {
+            'global_horizontal': "u",
+            'direct_normal' : "u",
+            'diffuse_horizontal' : "u",
+            'pr1_temperature' : "u",
+            'ph1_temperature': "u",
+            'pressure' : "u",
+            'zenith_angle' : "u",
+            'azimuth_angle' : "u",
+            'razon_status' : "u",
+            'razon_time' : "u",
+            'logger_battery' : "u",
+            'logger_temp' : "u",
+        }
+    }
--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-
-import LiveIrradianceLogic from './LiveIrradianceLogic'
-import LiveIrradianceCard from './LiveIrradiance'
 
 /**
  * import individual components
@@ -14,67 +10,77 @@ import LiveIrradianceCard from './LiveIrradiance'
 //  import { Link } from “react-router-dom”;
 
 //todo change to only import individual components
-import {Button, Alert, Breadcrumb, Card, ListGroup, ListGroupItem} from 'react-bootstrap';
-// import Button from 'react-bootstrap/Button';
-// import Alert from 'react-bootstrap/Alert';
-
-// class App extends React.Component{
-
-//   state={
-//     curTime : new Date().toLocaleString(),
-//   }
-
-//   render()  {
-//     return (
-//       <div className="App">
-//       <header className="App-header">
-//         {/* <img src={logo} className="App-logo" alt="logo" style={{width:"50px",height:"50px"}}/> */}
-//         <h1 id="header-text">Dashboard</h1>
-//       </header>
-
-//       <main className="App-main">
-
-//         <section className="App-main-header">
-//           <h1>UR Optics Solar Radiation Research Laboratory (RaZON+)</h1>
-//           <h1>Current Irradiance and Meteorological Conditions</h1>
-//           <hr/>
-//         </section>
-
-//         <section className="App-main-live" style={{marginTop:"24px"}}>
-//           <h2>Live Measurements</h2>
-//           <p>The current time is {this.state.curTime}.</p>
- 
-//           <Alert variant="success">Test Button</Alert>
-//           <Button>Test Button</Button>
-//           {/* <p>The current time is {currentTime}.</p> */}
-//         </section>
-
-//       </main>
-
-
-//     </div>
-//     );
-//   }
-
-// }
-
-// export default App;
+import { Button, Alert, Card, ListGroup, ListGroupItem, Row, Col, Container,} from 'react-bootstrap';
 
 function App() {
-  const [currentTime, setCurrentTime] = useState(0);
+  const tempData = [
+    {
+      "irradiance": {
+        "diffuse_horizontal": 3,
+        "direct_normal": 3,
+        "global_horizontal": 3
+      },
+      "meteorological": {
+        "azimuth_angle": 3,
+        "logger_battery": 3,
+        "logger_temp": 3,
+        "ph1_temperature": 3,
+        "pr1_temperature": 3,
+        "pressure": 3,
+        "razon_status": 3,
+        "razon_time": 3,
+        "zenith_angle": 3
+      },
+      "time": "Fri, 25 Jun 2021 14:41:59 GMT",
+      "units": {
+        "azimuth_angle": "u",
+        "diffuse_horizontal": "u",
+        "direct_normal": "u",
+        "global_horizontal": "u",
+        "logger_battery": "u",
+        "logger_temp": "u",
+        "ph1_temperature": "u",
+        "pr1_temperature": "u",
+        "pressure": "u",
+        "razon_status": "u",
+        "razon_time": "u",
+        "zenith_angle": "u"
+      }
+    }
+  ]
 
-  const list = LiveIrradianceLogic();
+  const [solarData, setSoloarData] = useState([]);
+
+  
+  /**
+   * Obtain data from /data endpoint (similar json as tempData)
+   */
+  const getData = () => {
+    fetch('/data')
+      .then(function (response) {
+        console.log(response)
+        return response.json();
+      })
+      .then(function (myJson) {
+        console.log(myJson);
+        setSoloarData([myJson])
+      });
+  }
 
   useEffect(() => {
-    fetch('/time').then(res => res.json()).then(data => {
-      setCurrentTime(data.time);
-    });
-  }, []);
+    getData() // initial data
+
+    const interval = setInterval(() => { //every 1 minute will request for the data again
+      getData()
+    }, 60 * 1000)
+
+
+    return () => clearInterval(interval)
+  }, [])
 
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" style={{width:"50px",height:"50px"}}/> */}
         <h1 id="header-text">Dashboard</h1>
       </header>
 
@@ -82,57 +88,139 @@ function App() {
         <section className="App-main-header">
           <h1>UR Optics Solar Radiation Research Laboratory (RaZON+)</h1>
           <h1>Current Irradiance and Meteorological Conditions</h1>
-          <hr/>
+          <hr />
         </section>
 
-        <section className="App-main-live" style={{marginTop:"24px"}}>
+        <section className="App-main-live" style={{ marginTop: "24px" }}>
           <h2>Live Measurements</h2>
-          <p>The current time is {currentTime}.</p>
-
-          <div className="App-main-live-cards" style={{flexDirection:"row"}}>
-            <LiveIrradianceCard/>
-
-            <Card style={{ width: '18rem' , margin: '16px'}}>
+          {/* <p>The current time is {currentTime}.</p> */}
+          <Container style={{marginTop:"24px"}}>
+          <Row>
+          <Col>
+            <Card style={{width:'18rem'}}>
               <Card.Body>
-                <Card.Title>Irradiance</Card.Title>
+                <Card.Title><h3>Irradiance</h3></Card.Title>
               </Card.Body>
-              <ListGroup className="list-group-flush">
-                <ListGroupItem><a target="_blank" href="https://google.com">
-                  Cras justo odio</a>
-                </ListGroupItem>
-                <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-                <ListGroupItem>Vestibulum at eros</ListGroupItem>
-                {list.map(function(item) {
-                  return <ListGroupItem key={item}>{item}</ListGroupItem>;
-                })}
-              </ListGroup>
+
+              // todo maybe somehow make this into another functional component, prob not too important though
+              {solarData.map(item => {
+                return (
+                  <ListGroup className="list-group-flush">
+                    <ListGroupItem key={"item.irradiance.global_horizontal"}>
+                      <div class="d-flex justify-content-between">
+                        <div style={{ fontWeight: 'bolder' }}>global_horizontal</div>
+                        <div>{item.irradiance.global_horizontal} <small style={{ fontWeight: 'lighter' }}> W/m<sup>2</sup></small></div>
+                      </div>
+                    </ListGroupItem>
+                    <ListGroupItem key={"item.irradiance.direct_normal"}>
+                      <div class="d-flex justify-content-between">
+                        <div style={{ fontWeight: 'bolder' }}>direct_normal</div>
+                        <div>{item.irradiance.direct_normal} <small style={{ fontWeight: 'lighter' }}> W/m<sup>2</sup></small></div>
+                      </div>
+                    </ListGroupItem>
+                    <ListGroupItem key={"item.irradiance.diffuse_horizontal"}>
+                      <div class="d-flex justify-content-between">
+                        <div style={{ fontWeight: 'bolder' }}>diffuse_horizontal</div>
+                        <div>{item.irradiance.diffuse_horizontal} <small style={{ fontWeight: 'lighter' }}> W/m<sup>2</sup></small></div>
+                      </div>
+                    </ListGroupItem>
+                  </ListGroup>
+                );
+              })}
+
               <Card.Body>
                 {/* empty card body */}
                 {/* <Card.Link href="#">Card Link</Card.Link>
                 <Card.Link href="#">Another Link</Card.Link> */}
               </Card.Body>
             </Card>
+          </Col>
 
-            <Card style={{ width: '18rem', margin: '16px' }}>
+          <Col>
+          <Card style={{width:'18rem'}}>
               <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
+                <Card.Title><h3>Meteorological</h3></Card.Title>
+                {/* <Card.Text>
                   Some quick example text to build on the card title and make up the bulk of
                   the card's content.
-                </Card.Text>
+                </Card.Text> */}
               </Card.Body>
-              <ListGroup className="list-group-flush">
+              {solarData.map(item => {
+                return (
+                  <ListGroup className="list-group-flush">
+                    <ListGroupItem key={"item.meteorological.pr1_temperature"}>
+                      <div class="d-flex justify-content-between">
+                        <div style={{ fontWeight: 'bolder' }}>pr1_temperature</div>
+                        <div>{item.meteorological.pr1_temperature} &#176;<small style={{ fontWeight: 'lighter' }}>C</small></div>
+                      </div>
+                    </ListGroupItem>
+                    <ListGroupItem key={"item.meteorological.ph1_temperature"}>
+                      <div class="d-flex justify-content-between">
+                        <div style={{ fontWeight: 'bolder' }}>ph1_temperature</div>
+                        <div>{item.meteorological.ph1_temperature} &#176;<small style={{ fontWeight: 'lighter' }}>C</small></div>
+                      </div>
+                    </ListGroupItem>
+                    <ListGroupItem key={"item.meteorological.pressure"}>
+                      <div class="d-flex justify-content-between">
+                        <div style={{ fontWeight: 'bolder' }}>pressure</div>
+                        <div>{item.meteorological.pressure} <small style={{ fontWeight: 'lighter' }}>mBar</small></div>
+                      </div>
+                    </ListGroupItem>
+                    <ListGroupItem key={"item.meteorological.zenith_angle"}>
+                      <div class="d-flex justify-content-between">
+                        <div style={{ fontWeight: 'bolder' }}>zenith_angle</div>
+                        <div>{item.meteorological.zenith_angle} &#176;<small style={{ fontWeight: 'lighter' }}></small></div>
+                      </div>
+                    </ListGroupItem>
+                    <ListGroupItem key={"item.meteorological.azimuth_angle"}>
+                      <div class="d-flex justify-content-between">
+                        <div style={{ fontWeight: 'bolder' }}>azimuth_angle</div>
+                        <div>{item.meteorological.azimuth_angle} &#176;<small style={{ fontWeight: 'lighter' }}></small></div>
+                      </div>
+                    </ListGroupItem>
+                    <ListGroupItem key={"item.meteorological.razon_status"}>
+                      <div class="d-flex justify-content-between">
+                        <div style={{ fontWeight: 'bolder' }}>razon_status</div>
+                        <div>{item.meteorological.razon_status} <small style={{ fontWeight: 'lighter' }}></small></div>
+                      </div>
+                    </ListGroupItem>
+                    <ListGroupItem key={"item.meteorological.razon_time"}>
+                      <div class="d-flex justify-content-between">
+                        <div style={{ fontWeight: 'bolder' }}>razon_time</div>
+                        <div>{item.meteorological.razon_time} <small style={{ fontWeight: 'lighter' }}>hhmm</small></div>
+                      </div>
+                    </ListGroupItem>
+                    <ListGroupItem key={"item.meteorological.logger_battery"}>
+                      <div class="d-flex justify-content-between">
+                        <div style={{ fontWeight: 'bolder' }}>logger_battery</div>
+                        <div>{item.meteorological.logger_battery} <small style={{ fontWeight: 'lighter' }}>VDC</small></div>
+                      </div>
+                    </ListGroupItem>
+                    <ListGroupItem key={"item.meteorological.logger_temp"}>
+                      <div class="d-flex justify-content-between">
+                        <div style={{ fontWeight: 'bolder' }}>logger_temp</div>
+                        <div>{item.meteorological.logger_temp} &#176;<small style={{ fontWeight: 'lighter' }}>C</small></div>
+                      </div>
+                    </ListGroupItem>
+                    
+                  </ListGroup>
+                );
+              })}
+
+              {/* <ListGroup className="list-group-flush">
                 <ListGroupItem>Cras justo odio</ListGroupItem>
                 <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
                 <ListGroupItem>Vestibulum at eros</ListGroupItem>
-              </ListGroup>
+              </ListGroup> */}
               <Card.Body>
                 {/* <Card.Link href="#">Card Link</Card.Link>
                 <Card.Link href="#">Another Link</Card.Link> */}
               </Card.Body>
             </Card>
-          </div>
- 
+          </Col>
+          </Row>
+          </Container>
+          
           <Alert variant="success">Test Button</Alert>
           <Button>Test Button</Button>
           {/* <p>The current time is {currentTime}.</p> */}
