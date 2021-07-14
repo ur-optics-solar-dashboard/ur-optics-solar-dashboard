3a4641c45c813c4716f08759025beb6d4b4392c0
Jimmy Jinghao Yao <46383546+Haowjy@users.noreply.github.com>
2021-07-14 14:54:40 -0400

    header options

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,33 @@
 import { useHistory, useLocation } from "react-router-dom";
+import { Nav } from "react-bootstrap";
 
 const Header = (props) => {
   let history = useHistory()
   let location = useLocation()
   return (
     <header className={props.className}>
-      <h1 
-      id="header-text"
-      style={{cursor: "pointer"}}
-      onClick={() => {
-        console.log(location.pathname)
-        if(location.pathname !=="/"){
-          history.push('/')
-        }
-      }}>
+      {/* <h1
+        id="header-dashboard"
+        style={{ cursor: "pointer" }}
+        onClick={() => {
+          console.log(location.pathname)
+          if (location.pathname !== "/") {
+            history.push('/')
+          }
+        }}>
         Dashboard
-      </h1>
+      </h1> */}
+      <Nav
+        activeKey="/"
+        id="header-nav">
+
+        <Nav.Item>
+          <Nav.Link style={{color:"white", fontSize:32, paddingLeft:0, fontWeight:"bold"}} href="/">Dashboard</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link style={{color:"white", fontSize:32, fontWeight:"normal"}} href="/graph">Graph</Nav.Link>
+        </Nav.Item>
+      </Nav>
     </header>
   )
 }
